125790a2634a4f90d6ba3c425d46f7a996798492
Jonathan Sevold <Jmsevold@gmail.com>
2014-02-25 19:30:43 -0800

    first commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,20 @@
+class PagesController < ApplicationController
+	def home
+	end
+
+	def about
+	end
+	
+	def calendar
+	end
+
+	def contract
+	end
+
+	def contact
+	end
+
+	def links
+	end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Garvey::Application.config.secret_token = '9158bca8e1166db4cbadf2e55a13e696f4feef651e002b71c335013e016823ec8d52548a27a8cf698cf8e8a3a50629d8fdb3309309e39fa009c1ff8a7492ef7b'
